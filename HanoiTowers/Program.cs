4fcb05c418fef2af13e9750af17e8b112b710585
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2022-03-19 13:55:45 +0300

    Visualization has been improved

--- HanoiTowers/Program.cs
+++ HanoiTowers/Program.cs
@@ -7,7 +7,7 @@ namespace HanoiTowers
         //Program boyunca sabit olacak olan değişkenleri class dışında statik ve public olarak tanımlıyorum.
         //Console çıktısı için iki boyutlu bir dizi tanımlıyorum.
         public static int[,] kuleler;
-        public static int sutunlar;
+        public static int sutunlar = 3;
         public static int diskler;
         public static int adımSayısı = 0;
         static void Main(string[] args)
@@ -22,14 +22,28 @@ namespace HanoiTowers
             //for döngüsü ile ilk kuleye kullanıcının girdiği diskleri koyuyoruz
             for (int disk = 0; disk < diskler; disk++)
             {
-                kuleler[diskler, 0] = (disk * 2) - 1; 
+                kuleler[disk, 0] = (disk * 2) + 1;
             }
 
-            //printTowers();
-            //solveTowers
-            //scancllose
-            //syso(hareket sayısı)
+            
+            kuleleriYaz();
+            kuleleriÇöz(diskler, 0,2);
+            Console.WriteLine("Adım Sayısı : " + adımSayısı);
 
+        }
+
+        public static void kuleleriÇöz(int diskNo, int eskiSutun, int yeniSutun )
+        {
+            int ortaSutun = sutunlar - yeniSutun - eskiSutun;
+            if (diskNo == 0)
+            {
+
+            } else
+            {
+                kuleleriÇöz(diskNo - 1, eskiSutun, ortaSutun);
+                diskHareketi(eskiSutun, yeniSutun);
+                kuleleriÇöz(diskNo - 1, ortaSutun, yeniSutun);
+            } 
         }
 
         //burada disk hareketini tanımlıyorum
@@ -37,7 +51,7 @@ namespace HanoiTowers
         {
             adımSayısı++;
             int disk = 0;
-            while (disk < diskler && kuleler[disk,eski] == 0)
+            while (disk < diskler && kuleler[disk, eski] == 0)
             {
                 disk++;
             }
@@ -47,15 +61,60 @@ namespace HanoiTowers
 
 
             int yeniDisk = 0;
-            while (yeniDisk < diskler && kuleler [disk,yeni] == 0 )
+            while (yeniDisk < diskler && kuleler[yeniDisk, yeni] == 0)
             {
                 yeniDisk++;
             }
 
             kuleler[--yeniDisk, yeni] = temp;
-            
 
+            kuleleriYaz();
         }
 
+        //burada kuleleri ekrana yazıyoruz  "bir" kule
+        public static void kuleleriYaz()
+        {
+            for (int disk = 0; disk < diskler; disk++)
+            {
+                for (int sutun = 0; sutun < sutunlar; sutun++)
+                {
+
+                    Console.Write(aralık(kuleler[disk, sutun]));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        //Console ekranında görünüm iyileştirmesi için
+        public static string aralık(int disk)
+        {
+
+            int sutunGenisligi = (diskler * 2) + 2;
+
+            string cıkıs = "";
+            if (disk == 0)
+            {
+                cıkıs = "|";
+            }
+            else
+            {
+                for (int i = 0; i < disk; i++)
+                {
+                    cıkıs += "*";
+                }
+
+            }
+            if (cıkıs.Length % 2 == 1)
+            {
+                cıkıs += " ";
+            }
+
+            while (cıkıs.Length < sutunGenisligi)
+            {
+                cıkıs = " " + cıkıs + " ";
+            }
+            return cıkıs;
+        }
     }
 }
