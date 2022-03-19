7e5051d283a2ec8be9eaae28f785ac1e2dfc8176
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2022-03-19 10:17:37 +0300

    Disks taken from user put in tower

--- HanoiTowers/Program.cs
+++ HanoiTowers/Program.cs
@@ -4,9 +4,29 @@ namespace HanoiTowers
 {
     class Program
     {
+        //Program boyunca sabit olacak olan değişkenleri class dışında statik ve public olarak tanımlıyorum.
+        //Console çıktısı için iki boyutlu bir dizi tanımlıyorum.
+        public static int[,] kuleler;
+        public static int sutunlar;
+        public static int diskler;
+        public static int adımSayısı = 0;
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Disk sayısını giriniz");
+            //Kullanıcıdan disk sayısı alınıyor 
+            int deger = int.Parse(Console.ReadLine());
+            diskler = deger;
+            //İki boyutlu diziye diskleri ve sütunları atıyorum.
+            kuleler = new int[diskler, sutunlar];
+
+            //for döngüsü ile ilk kuleye kullanıcının girdiği diskleri koyuyoruz
+            for (int disk = 0; disk < diskler; disk++)
+            {
+                kuleler[diskler, 0] = (disk * 2) - 1; 
+            }
+
+            
+
         }
 
 
