9c41bc14452b73ecb2ed522b2767fd204d3b9dbf
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2022-03-19 15:07:38 +0300

    Code description added

--- HanoiTowers/Program.cs
+++ HanoiTowers/Program.cs
@@ -19,97 +19,109 @@ namespace HanoiTowers
             //İki boyutlu diziye diskleri ve sütunları atıyorum.
             kuleler = new int[diskler, sutunlar];
 
-            //for döngüsü ile ilk kuleye kullanıcının girdiği diskleri koyuyoruz
+            //for döngüsü ile diskin uzunluğu belirtiliyor ve satırlara atanıyor
             for (int disk = 0; disk < diskler; disk++)
             {
                 kuleler[disk, 0] = (disk * 2) + 1;
             }
 
-            
+            //Dış fonksiyonları Main fonksiyonuna çağırıyorum
             kuleleriYaz();
-            kuleleriÇöz(diskler, 0,2);
+            kuleleriÇözümle(diskler, 0, 2);
             Console.WriteLine("Adım Sayısı : " + adımSayısı);
-
         }
 
-        public static void kuleleriÇöz(int diskNo, int eskiSutun, int yeniSutun )
+        //Recurisive fonksiyonum
+        public static void kuleleriÇözümle(int diskNo, int eskiSutun, int yeniSutun)
         {
+            //Orta sütunun int değeri diğer sütunların değerine göre atanıyor
             int ortaSutun = sutunlar - yeniSutun - eskiSutun;
             if (diskNo == 0)
             {
-
-            } else
+                //İlk durum disk yok ise bu satır çalışmayacak
+            }
+            else
             {
-                kuleleriÇöz(diskNo - 1, eskiSutun, ortaSutun);
+                //Disk sayısı bir azaltılarak gönderilen parametrelere göre eski sütundan orta sütuna diskler hareket ediyor
+                kuleleriÇözümle(diskNo - 1, eskiSutun, ortaSutun);
+                //Diskler hareket ettiriliyor
                 diskHareketi(eskiSutun, yeniSutun);
-                kuleleriÇöz(diskNo - 1, ortaSutun, yeniSutun);
-            } 
+                //Disk sayısı bir azaltılarak gönderilen parametrelere göre orta sütundan yeni sütuna diskler hareket ediyor
+                kuleleriÇözümle(diskNo - 1, ortaSutun, yeniSutun);
+            }
         }
 
         //burada disk hareketini tanımlıyorum
         public static void diskHareketi(int eski, int yeni)
         {
+            //Fonksiyon her çağrıldığında adım sayısı bir artıyor
             adımSayısı++;
             int disk = 0;
+            //Kullanıcının girdiği disk sayısı kontrol ediliyor ve iki boyutlu dizinin ilgili değerinin 0 olması isteniyor.
             while (disk < diskler && kuleler[disk, eski] == 0)
             {
                 disk++;
             }
-
+            //Temp değişkeni ile kuleler dizisinin referansını tutuyorum
             int temp = kuleler[disk, eski];
             kuleler[disk, eski] = 0;
 
-
             int yeniDisk = 0;
+            //Kullanıcının girdiği disk sayısı kontrol ediliyor ve iki boyutlu dizinin ilgili değerinin 0 olması isteniyor.
             while (yeniDisk < diskler && kuleler[yeniDisk, yeni] == 0)
             {
                 yeniDisk++;
             }
-
+            //Tuttuğum referansı dizime geri atıyorum ve disklerin yeri değişmiş oluyor
+            //Out of bounds hatası almamak için yeniDisk değişkenini bir azaltıyorum
             kuleler[--yeniDisk, yeni] = temp;
-
+            //kuleleriYaz fonksiyonu çağırılıyor
             kuleleriYaz();
         }
 
-        //burada kuleleri ekrana yazıyoruz  "bir" kule
+        //burada kuleleri ve diskleri ekrana yazıyoruz
         public static void kuleleriYaz()
         {
             for (int disk = 0; disk < diskler; disk++)
             {
                 for (int sutun = 0; sutun < sutunlar; sutun++)
                 {
-
+                    //Kule veya diskin console a yazıldığı yer
                     Console.Write(aralık(kuleler[disk, sutun]));
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine();
+            Console.WriteLine("-----------------------------------");
         }
 
         //Console ekranında görünüm iyileştirmesi için
         public static string aralık(int disk)
         {
-
+            //Kuleler arası mesafe
             int sutunGenisligi = (diskler * 2) + 2;
-
+            //ilk basım değeri
             string cıkıs = "";
+            //Disk yok ise | işareti koyuluyor
             if (disk == 0)
             {
+                //Temsili sütun
                 cıkıs = "|";
             }
             else
             {
+                //Kullanıcı girdisi kadar + işareti koyuluyor
                 for (int i = 0; i < disk; i++)
                 {
-                    cıkıs += "*";
+                    //Temsili disk
+                    cıkıs += "+";
                 }
-
             }
+            //Kulenin sağına koyulan boşluk
             if (cıkıs.Length % 2 == 1)
             {
                 cıkıs += " ";
             }
-
+            //Disklerin sol ve sağ taraflarına koyulan boşluk
             while (cıkıs.Length < sutunGenisligi)
             {
                 cıkıs = " " + cıkıs + " ";
