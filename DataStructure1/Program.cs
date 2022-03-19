17ee5d38dbc55f4382cd350b9044023c82db8e64
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2022-03-19 09:15:35 +0300

    Add project files.

---
+++ DataStructure1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataStructure1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int sayi [] = new int[a];
+
+           
+        }
+    }
+}
---
+++ DersDeneme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DersDeneme
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int sum = 0; 
+        //        for (int i = 1; i <= 4; i = i * 3)
+        //        for (int j = 1; j <= i; j++)
+        //        sum++;
+
+        //    Console.WriteLine(sum);
+        //}
+
+        
+
+
+        
+
+
+    }
+}
+
+
+
+
+
