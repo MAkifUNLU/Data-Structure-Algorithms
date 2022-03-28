8e1ff07ee525e6a973a1833fdd441fcefea5ab91
Akif Ünlü <akif.unlu44@gmail.com>
2022-03-28 17:30:45 +0300

    Personel class created

---
+++ Personnel_Information/Personel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personnel_Information
+{
+    public class Personel
+    {
+        public string name { get; set; }
+        public string sureName { get; set; }
+        public int registrationNumber { get; set; }
+        public int salary { get; set; }
+    }
+}
