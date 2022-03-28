9d85bdba8bb5970d363d197d6b4d3cde1d31d386
Akif Ünlü <akif.unlu44@gmail.com>
2022-03-28 17:29:46 +0300

    Project Started

---
+++ Personnel_Information/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Personnel_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel personel1 = new Personel { name = "Akif", sureName = "Ünlü", registrationNumber = 1, salary = 4000 };
+            Personel personel2 = new Personel { name= "Selçuk", sureName= "Bayraktar", registrationNumber = 2, salary= 5000};
+            Personel personel3 = new Personel { name = "Barış", sureName = "Özcan", registrationNumber = 3, salary = 6000 };
+            Personel personel4 = new Personel { name = "Neşet", sureName = "Ertaş", registrationNumber = 4, salary = 7000 };
+        }
+
+
+    }
+}
