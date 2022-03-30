ade9d5630751b76c896e140991d5294e18e283d6
Akif Ünlü <akif.unlu44@gmail.com>
2022-03-30 22:26:09 +0300

    Personel class added

--- Personnel_Information/Program.cs
+++ Personnel_Information/Program.cs
@@ -6,12 +6,27 @@ namespace Personnel_Information
     {
         static void Main(string[] args)
         {
-            Personel personel1 = new Personel { name = "Akif", sureName = "Ünlü", registrationNumber = 1, salary = 4000 };
-            Personel personel2 = new Personel { name= "Selçuk", sureName= "Bayraktar", registrationNumber = 2, salary= 5000};
-            Personel personel3 = new Personel { name = "Barış", sureName = "Özcan", registrationNumber = 3, salary = 6000 };
-            Personel personel4 = new Personel { name = "Neşet", sureName = "Ertaş", registrationNumber = 4, salary = 7000 };
+
+            Personel person1 = new Personel("Akif", "ÜNLÜ", 9000);
+            Personel person2 = new Personel("Barış", "ÖZCAN", 2000);
+            Personel person3 = new Personel("Selçuk", "BAYRAKTAR", 8000);
+            Personel person4 = new Personel("Neşet", "ERTAŞ", 3000);
+            Personel person5 = new Personel("Şatlık", "MURADOV", 3000);
+
+            LinkedList list = new LinkedList();
+
+            list.addNode(person1);
+            list.addNode(person2);           
+            list.addNode(person3);
+            list.addNode(person4);
+            list.addNode(person5);
+
+            //list.arrangement();
+            list.listElements();
+            Console.WriteLine("--------------------");
+            list.find(2);
+            
+            
         }
-
-
     }
 }
