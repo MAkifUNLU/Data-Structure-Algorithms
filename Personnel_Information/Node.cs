6f367747f333ae0fddbbb3a68736444dd8746630
Akif Ünlü <akif.unlu44@gmail.com>
2022-03-30 22:24:40 +0300

    Node class added

---
+++ Personnel_Information/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personnel_Information
+{
+    class Node
+    {
+        public Personel person;
+        public Node next;
+
+        public Node(Personel person)
+        {
+            this.person = person;
+            next = null;
+        }
+    }
+
+    
+}
