467adacb6d6e82d72251718c503d9864bbbe6829
Akif Ünlü <akif.unlu44@gmail.com>
2022-03-30 22:25:26 +0300

    Personel class added. Methods created

--- Personnel_Information/Personel.cs
+++ Personnel_Information/Personel.cs
@@ -6,9 +6,30 @@ namespace Personnel_Information
 {
     public class Personel
     {
+        private static int tmpId;
+
+        public Personel(string name, string sureName , int salary)
+        {
+            this.name = name;
+            this.sureName = sureName;
+            this.registrationNumber = registrationNumber;
+            this.salary = salary;
+            tmpId++;
+            this.registrationNumber = tmpId;
+        }
+
         public string name { get; set; }
         public string sureName { get; set; }
         public int registrationNumber { get; set; }
         public int salary { get; set; }
+
+        public void printPerson()
+        {
+            Console.WriteLine("İsim:" + name);
+            Console.WriteLine("Soyisim:" + sureName);
+            Console.WriteLine("ID:" + registrationNumber);
+            Console.WriteLine("Maaş:" + salary);
+        }
     }
+   
 }
