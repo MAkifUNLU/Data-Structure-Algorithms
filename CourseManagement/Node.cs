ac7b5059cc470485bcfa967b2b4b91195c9bc096
Akif Ünlü <akif.unlu44@gmail.com>
2022-04-04 09:33:37 +0300

    Node class added

---
+++ CourseManagement/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagement
+{
+    //           ÇİFT YÖNLÜ(DOUBLY) LİSTELER İÇİN NODE
+    class NodeL<T>
+    {
+        public NodeL<T> next;
+        public T _data;
+
+        public NodeL(T data)
+        {
+            _data = data;
+            next = null;
+        }
+    }
+    //           TEK YÖNLÜ(SİNGLY) LİSTELER İÇİN NODE
+    class NodeC<T>
+    {
+        public NodeC<T> next;
+        public NodeC<T> prev;
+        public T _data;
+
+        public NodeC(T data)
+        {
+            _data = data;
+            next = null;
+        }
+    }
+
+}
