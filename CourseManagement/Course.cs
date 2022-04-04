a263ec73d94e23c8fee9e42e4a943653320768bd
Akif Ünlü <akif.unlu44@gmail.com>
2022-04-04 09:32:12 +0300

    Project Created

---
+++ CourseManagement/Course.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagement
+{
+    class Course
+    {
+        private static int codeId;
+
+        public string courseName { get; set; }
+        public int code { get; set; }
+        public int kredi { get; set; }
+        public int akts { get; set; }
+        public string lecturerName { get; set; }
+        public string lecturerSurename { get; set; }
+
+        public void printCours()
+        {
+            Console.WriteLine("Ders Kodu:" + code);
+            Console.WriteLine("Ders İsmi:" + courseName);
+            Console.WriteLine("Hocanın adı, soyadı:" + lecturerName + " " + lecturerSurename);
+        }
+
+        public Course(string courseName, int code, int kredi, int akts, string lecturerName, string lecturerSurename)
+        {
+            this.courseName = courseName;
+            this.code = code;
+            codeId++;
+            this.code = codeId;
+            this.kredi = kredi;
+            this.akts = akts;
+            this.lecturerName = lecturerName;
+            this.lecturerSurename = lecturerSurename;
+        }
+    }
+    
+}
---
+++ CourseManagement/Program.cs
@@ -0,0 +1,59 @@
+﻿using CourseManagement.LinkedList;
+using System;
+
+namespace CourseManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                          TEK YÖNLÜ DOĞRUSAL LİSTE İÇİN
+
+            //SON ELEMANI SİLME
+            //Course course1 = new Course("Data Structures",101,3,4,"Adem", "TEKEREK" );
+            //Course course2 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
+            //Course course3 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course4 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
+
+            //OneWayLineerLinkedList list1 = new OneWayLineerLinkedList();
+            
+            //list1.addFront(course1);
+            //list1.addBack(course2);
+            //list1.addBack(course3);
+            //list1.addBack(course4);
+            //list1.removeFirst();
+            //list1.removeLast();
+            //list1.removeById(3);
+            //list1.listElements();
+            //list1.findCourseById(4);
+            //list1.findCoursByName("Data Base");
+
+
+            //                              ÇİFT YÖNLÜ DOĞRUSAL İÇİN
+
+            //Course course5 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
+            //Course course6 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
+            //Course course7 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course8 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
+            //arama, ekleme, silme, yazdırma
+            //---------------------------------------------------------------------------------------------
+            //tek yönlü dairesel için
+            //Course course9 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
+            //Course course10 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
+            //Course course11 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course12 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
+
+            //OneWayCircleLinkedList list2 = new OneWayCircleLinkedList();
+            //arama, ekleme, silme, yazdırma
+            //---------------------------------------------------------------------------------------------
+
+
+            //çift yönlü dairesel için
+            //Course course13 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
+            //Course course14 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
+            //Course course15 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course16 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
+            //arama, ekleme, silme, yazdırma
+        }
+    }
+}
