4c8830205fc4bd2c4a74b907f009c969c9df96db
Akif Ünlü <akif.unlu44@gmail.com>
2022-04-04 16:44:32 +0300

    Method calls

--- CourseManagement/Program.cs
+++ CourseManagement/Program.cs
@@ -7,16 +7,16 @@ namespace CourseManagement
     {
         static void Main(string[] args)
         {
-            //                          TEK YÖNLÜ DOĞRUSAL LİSTE İÇİN
+            // ------------------------ TEK YÖNLÜ DOĞRUSAL İÇİN --------------------------
 
-            //SON ELEMANI SİLME
+            //SON ELEMANI SİLMEYİ YAP
             //Course course1 = new Course("Data Structures",101,3,4,"Adem", "TEKEREK" );
             //Course course2 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
             //Course course3 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
             //Course course4 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
 
             //OneWayLineerLinkedList list1 = new OneWayLineerLinkedList();
-            
+
             //list1.addFront(course1);
             //list1.addBack(course2);
             //list1.addBack(course3);
@@ -28,27 +28,33 @@ namespace CourseManagement
             //list1.findCourseById(4);
             //list1.findCoursByName("Data Base");
 
-
-            //                              ÇİFT YÖNLÜ DOĞRUSAL İÇİN
+            // ------------------------ ÇİFT YÖNLÜ DOĞRUSAL İÇİN --------------------------
 
             //Course course5 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
             //Course course6 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
             //Course course7 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
             //Course course8 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
             //arama, ekleme, silme, yazdırma
-            //---------------------------------------------------------------------------------------------
-            //tek yönlü dairesel için
+
+            // ------------------------ TEK YÖNLÜ DAİRESEL İÇİN --------------------------
+
             //Course course9 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
             //Course course10 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
             //Course course11 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
             //Course course12 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
 
-            //OneWayCircleLinkedList list2 = new OneWayCircleLinkedList();
-            //arama, ekleme, silme, yazdırma
-            //---------------------------------------------------------------------------------------------
+            //OneWayCircularLinkedList list2 = new OneWayCircularLinkedList();
+            ////+arama, +ekleme(sona ekliyor), +silme, +yazdırma
+            //list2.addBack(course9);
+            //list2.addBack(course10);
+            //list2.addBack(course11);
+            //list2.addBack(course12);
+            //list2.removeById(2);
+            ////list2.findCourseById(4);
+            ////list2.findCoursByName("Data Base");
+            //list2.listElements();
 
-
-            //çift yönlü dairesel için
+            // ------------------------ ÇİFT YÖNLÜ DAİRESEL İÇİN --------------------------
             //Course course13 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
             //Course course14 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
             //Course course15 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
