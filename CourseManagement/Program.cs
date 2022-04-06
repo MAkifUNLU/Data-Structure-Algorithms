8a2ceadef5a11b53c289e48758619ad4e702e236
Akif Ünlü <akif.unlu44@gmail.com>
2022-04-06 22:27:19 +0300

    Program.cs finish

--- CourseManagement/Program.cs
+++ CourseManagement/Program.cs
@@ -7,59 +7,93 @@ namespace CourseManagement
     {
         static void Main(string[] args)
         {
+
             // ------------------------ TEK YÖNLÜ DOĞRUSAL İÇİN --------------------------
 
-            //SON ELEMANI SİLMEYİ YAP
-            //Course course1 = new Course("Data Structures",101,3,4,"Adem", "TEKEREK" );
-            //Course course2 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
-            //Course course3 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
-            //Course course4 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
+            //Course course1 = new Course("Data Structures", 1, 3, 4, "Adem", "TEKEREK");
+            //Course course2 = new Course("Visual Programming", 2, 2, 3, "Ayhan", "ERDEM");
+            //Course course3 = new Course("Data Base", 3, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course4 = new Course("Web Programming", 4, 5, 3, "Sinan", "TOKLU");
 
             //OneWayLineerLinkedList list1 = new OneWayLineerLinkedList();
 
             //list1.addFront(course1);
-            //list1.addBack(course2);
-            //list1.addBack(course3);
-            //list1.addBack(course4);
+            //list1.addFront(course2);
+            //list1.addFront(course3);
+            //list1.addFront(course4);
+            //list1.insertAfter(3,course4);
             //list1.removeFirst();
             //list1.removeLast();
-            //list1.removeById(3);
+            ////list1.removeById(3);
             //list1.listElements();
             //list1.findCourseById(4);
             //list1.findCoursByName("Data Base");
 
+
             // ------------------------ ÇİFT YÖNLÜ DOĞRUSAL İÇİN --------------------------
 
-            //Course course5 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
-            //Course course6 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
-            //Course course7 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
-            //Course course8 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
-            //arama, ekleme, silme, yazdırma
+            //Course course5 = new Course("Data Structures", 1, 3, 4, "Adem", "TEKEREK");
+            //Course course6 = new Course("Visual Programming", 2, 2, 3, "Ayhan", "ERDEM");
+            //Course course7 = new Course("Data Base", 3, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course8 = new Course("Web Programming", 4, 5, 3, "Sinan", "TOKLU");
+
+            //DoublyLineerLinkedList list2 = new DoublyLineerLinkedList();
+
+            //list2.addBack(course5);
+            //list2.addBack(course6);
+            //list2.addBack(course7);
+            //list2.addBack(course8);
+            ////list2.insertAfter(2, course8);
+
+            ////list2.removeById(2);
+            ////list2.listElements();
+            //list2.removeLast();
+            //list2.removeFirst();
+            //list2.listElements();
+            //list2.findCourseById(2);
+            //list2.findCoursByName("Data Base");
+
 
             // ------------------------ TEK YÖNLÜ DAİRESEL İÇİN --------------------------
 
-            //Course course9 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
-            //Course course10 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
-            //Course course11 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
-            //Course course12 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
+            //Course course9 = new Course("Data Structures", 1, 3, 4, "Adem", "TEKEREK");
+            //Course course10 = new Course("Visual Programming", 2, 2, 3, "Ayhan", "ERDEM");
+            //Course course11 = new Course("Data Base", 3, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course12 = new Course("Web Programming", 4, 5, 3, "Sinan", "TOKLU");
 
-            //OneWayCircularLinkedList list2 = new OneWayCircularLinkedList();
-            ////+arama, +ekleme(sona ekliyor), +silme, +yazdırma
-            //list2.addBack(course9);
-            //list2.addBack(course10);
-            //list2.addBack(course11);
-            //list2.addBack(course12);
-            //list2.removeById(2);
-            ////list2.findCourseById(4);
-            ////list2.findCoursByName("Data Base");
-            //list2.listElements();
+            //OneWayCircularLinkedList list3 = new OneWayCircularLinkedList();
 
-            // ------------------------ ÇİFT YÖNLÜ DAİRESEL İÇİN --------------------------
-            //Course course13 = new Course("Data Structures", 101, 3, 4, "Adem", "TEKEREK");
-            //Course course14 = new Course("Visual Programming", 102, 2, 3, "Ayhan", "ERDEM");
-            //Course course15 = new Course("Data Base", 103, 4, 5, "Necattin", "BARIŞÇI");
-            //Course course16 = new Course("Web Programming", 104, 5, 3, "Sinan", "TOKLU");
-            //arama, ekleme, silme, yazdırma
+            //list3.addFront(course9);
+            //list3.addFront(course10);
+            //list3.addFront(course11);
+            //list3.addFront(course12);
+            ////list3.removeById(1);
+            ////list3.removeFirst();
+            ////list3.removeLast();
+            ////list3.findCourseById(4);
+            ////list3.findCoursByName("Data Base");
+            //list3.listElements();
+
+
+            //------------------------ÇİFT YÖNLÜ DAİRESEL İÇİN--------------------------
+
+            //Course course13 = new Course("Data Structures", 1, 3, 4, "Adem", "TEKEREK");
+            //Course course14 = new Course("Visual Programming", 2, 2, 3, "Ayhan", "ERDEM");
+            //Course course15 = new Course("Data Base", 3, 4, 5, "Necattin", "BARIŞÇI");
+            //Course course16 = new Course("Web Programming", 4, 5, 3, "Sinan", "TOKLU");
+
+            //DoublyCircularLinkedList list4 = new DoublyCircularLinkedList();
+
+            //list4.addFront(course13);
+            //list4.addFront(course14);
+            //list4.addFront(course15);
+            //list4.addFront(course16);
+            ////list4.insertAfter(1, course14);
+            ////list4.removeById(4);
+            //list4.listElements();
+            ////list4.findCoursByName("Data Base");
+            ////list4.findCourseById(2);
+
         }
     }
 }
